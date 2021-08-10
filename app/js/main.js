a8af48abbf09cd888bb65a109ae6c29233007205
Omgfrost1 <V-hutarcov@mail.com>
2021-08-10 07:40:46 +0300

    fix slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,10 @@ $(function () {
 
   $('.header-slider').slick({
     dots: false,
+    fade: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
     prevArrow: '<button type = "button" class= "header-slider__slick-prev"><img src="images/arrow-left.svg" alt="left arrow"> </button>',
     nextArrow: '<button type = "button" class= "header-slider__slick-next"><img src="images/arrow-right.svg" alt="right arrow"> </button>',
     responsive: [
